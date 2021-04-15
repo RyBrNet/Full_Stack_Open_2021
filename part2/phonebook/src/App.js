948d743a523ac79cf3d94c74a788600cd8171f5b
RyBrNet <9062274+RyBrNet@users.noreply.github.com>
2021-04-15 12:51:15 -0700

    Phonebook Step 6 Exercise 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,18 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios'
 import DisplayPeople from "./components/DisplayPeople.js";
 import Filter from "./components/Filter.js";
 import NewAddition from "./components/NewAddition.js";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", phoneNumber: "040-123456" },
-    { name: "Ada Lovelace", phoneNumber: "39-44-5323523" },
-    { name: "Dan Abramov", phoneNumber: "12-43-234345" },
-    { name: "Mary Poppendieck", phoneNumber: "39-23-6423122" },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newPhoneNumber, setNewPhoneNumber] = useState("");
   const [nameFilter, setNameFilter] = useState("");
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
 
   return (
     <div>
