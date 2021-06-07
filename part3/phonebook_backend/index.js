01d476e4f4b8b5db0cea3d820ef615a4be4170c6
RyBrNet <9062274+RyBrNet@users.noreply.github.com>
2021-06-07 14:34:04 -0700

    Phonebook fron & back combined

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static('build'))
 
 var morgan = require('morgan')
 morgan.token('phone_number', function getPhoneNumber(request) {
@@ -97,7 +98,7 @@ app.post('/api/persons', (request, response) => {
   response.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
